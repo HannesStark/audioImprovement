404b2386920e14fe1bce9ab92553f2b9d276dfaf
hannes-staerk <hannes.staerk@gmail.com>
2020-03-11 17:12:34 +0100

    remove halving from overlay with noise

--- temp.py
+++ temp.py
@@ -1,21 +1,23 @@
+import matplotlib.pyplot as plt
 import librosa
 import numpy as np
 
 import h5py
+from torchvision import transforms
 
 from datasets.audio_dataset import AudioDataset
 from datasets.hdf5_dataset import HDF5Dataset
-from utils import create_hdf5
+from datasets.segments_dataset import SegmentsDataset
+from datasets.transforms import Normalize, NoiseTransform, ToTensor
+from utils import create_hdf5, get_audio_list, train_val_split
 import soundfile as sf
 
-#noise_dataset = AudioDataset('F:/datasets/Nonspeech_SR16000')
-#create_hdf5('F:\datasets\libri_speech', noise_dataset, 20000)
+noise_dataset = AudioDataset('F:/datasets/Nonspeech_SR16000')
+create_hdf5('F:\datasets\libri_speech_subset', noise_dataset, 512)
 
-#f = h5py.File('F:\datasets/libri_speech_subset.hdf5', 'r')
-#sf.write("noisy.wav", f['data'][1][0], samplerate=16000)
-#sf.write("clean.wav", f['data'][1][1], samplerate=16000)
-#print(f['data'][1])
+# f = h5py.File('F:\datasets/libri_speech_subset.hdf5', 'r')
+# sf.write("noisy.wav", f['data'][1][0], samplerate=16000)
+# sf.write("clean.wav", f['data'][1][1], samplerate=16000)
+# print(f['data'][1])
 
-dataset = HDF5Dataset('F:\datasets/libri_speech_subset.hdf5')
-
-print(dataset[1])
+# dataset = HDF5Dataset('F:\datasets/libri_speech_subset.hdf5')
